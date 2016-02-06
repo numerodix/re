6db21834ae0a4d960b210b24598d6cf2e2eb90dd
Martin Matusiak <numerodix@gmail.com>
2016-02-06 21:53:16 +0100

    get rid of requirements.txt

--- setup.py
+++ setup.py
@@ -7,16 +7,6 @@ from setuptools import setup
 import reps
 
 
-def load_requirements():
-    path = os.path.dirname(__file__)
-    filepath = os.path.join(path, 'requirements.txt')
-    with open(filepath, 'rb') as f:
-        content = f.read()
-        lines = content.splitlines()
-        lines = [re.sub('#.*$', '', line) for line in lines if line.strip()]
-        return lines
-
-
 setup(
     name='reps',
     version=reps.__version__,
@@ -28,7 +18,10 @@ setup(
     packages=find_packages('.'),
     package_dir={'': '.'},
 
-    install_requires=load_requirements(),
+    install_requires=[
+        'ansicolor==0.2.4',
+        'ordereddict==1.1',
+    ],
 
     # don't install as zipped egg
     zip_safe=False,
